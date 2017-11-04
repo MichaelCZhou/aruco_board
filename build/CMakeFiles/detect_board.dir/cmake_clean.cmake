4cccc8b579c005da5c472dae9fdb5433eea14708
MichaelCZhou <1205464428@qq.com>
2017-11-04 14:26:55 +0800

    use aruco_board

---
+++ build/CMakeFiles/detect_board.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_board.dir/detect_board.cpp.o"
+  "detect_board.pdb"
+  "detect_board"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_board.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/detect_board_charuco.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_board_charuco.dir/detect_board_charuco.cpp.o"
+  "detect_board_charuco.pdb"
+  "detect_board_charuco"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_board_charuco.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
